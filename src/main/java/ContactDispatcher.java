7cf2e6b90ad93d10dd8b5c46f9d0f571d8841565
muyangye <muyangye@usc.edu>
2022-05-03 14:12:21 -0700

    chat dataflow

--- src/main/java/ContactDispatcher.java
+++ src/main/java/ContactDispatcher.java
@@ -54,6 +54,7 @@ public class ContactDispatcher extends HttpServlet {
 //    	message.insertIntoDatabase();
 //    	System.out.println(timestamp.getClass().getName());
 //    	System.out.println(Util.Constant.dateFormat.format(createdTime));
+    	request.setAttribute("name_to_messages", name_to_messages);
     	response.sendRedirect("contact.jsp");
     }
     
